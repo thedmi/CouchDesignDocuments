70decab32a11ce8b5208903dabeac477c4c5a9f0
Dani Michel <dani.michel@smartprimes.ch>
2014-03-20 17:05:23 +0100

    Abstract section extracted.

--- CouchDesignDocuments/ShowsSection.cs
+++ CouchDesignDocuments/ShowsSection.cs
@@ -1,16 +1,12 @@
 ﻿namespace TheDmi.CouchDesignDocuments
 {
-    using System;
     using System.Runtime.CompilerServices;
 
-    public abstract class ShowsSection<TSelf> : IShowsSection
+    public abstract class ShowsSection<TSelf> : StandardSection, IShowsSection
     {
         protected static FunctionSpec Show([CallerMemberName] string showFunctionName = null)
         {
-            return
-                new FunctionSpec(
-                    new Lazy<string>(
-                        () => SectionJsReader.ReadJsFromResources(showFunctionName, "Shows", typeof(TSelf))));
+            return SectionPart(showFunctionName, "Shows", typeof(TSelf));
         }
     }
 }
---
+++ CouchDesignDocuments/StandardSection.cs
@@ -0,0 +1,15 @@
+﻿namespace TheDmi.CouchDesignDocuments
+{
+    using System;
+
+    public abstract class StandardSection
+    {
+        protected static FunctionSpec SectionPart(string showFunctionName, string partNamespace, Type concreteSectionType)
+        {
+            return
+                new FunctionSpec(
+                    new Lazy<string>(
+                        () => SectionJsReader.ReadJsFromResources(showFunctionName, partNamespace, concreteSectionType)));
+        }
+    }
+}
