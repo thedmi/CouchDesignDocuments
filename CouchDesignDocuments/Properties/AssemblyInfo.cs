e955eaf076bc2df86b94b540e2bca99d3be4fca6
Dani Michel <dani.michel@smartprimes.ch>
2014-03-24 13:51:14 +0100

    Description added.

--- CouchDesignDocuments/Properties/AssemblyInfo.cs
+++ CouchDesignDocuments/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CouchDesignDocuments")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A CouchDB design document generator that integrates into your .NET application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("CouchDesignDocuments")]
 [assembly: AssemblyTrademark("")]
