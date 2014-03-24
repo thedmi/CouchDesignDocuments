1a09ce7c3950091fcaab87e21845a11222df26d0
Dani Michel <dani.michel@smartprimes.ch>
2014-03-24 13:42:15 +0100

    VersionInfo.cs extracted.

---
+++ VersionInfo.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VersionInfo.cs" company="Dani Michel">
+//   Dani Michel 2013
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Dani Michel")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyVersion("0.2.0.*")]
+[assembly: AssemblyInformationalVersion("Belt 0.2.0")]
