d8e1ff124c96ea695e73a653defc70ccdfe98a00
Dani Michel <dani.michel@smartprimes.ch>
2015-01-03 16:46:08 +0100

    Error messages improved.

--- Sources/CouchDesignDocuments/Resources/ResourceLocator.cs
+++ Sources/CouchDesignDocuments/Resources/ResourceLocator.cs
@@ -17,14 +17,18 @@
         public string GetResourceFqn(string name, params string[] requiredNamespaceParts)
         {
             var resourceNames = _sourceAssembly.GetManifestResourceNames();
-            var foundName = resourceNames.SingleOrDefault(n => MeetsRequirements(n, name, requiredNamespaceParts));
+            var foundNames = resourceNames.Where(n => MeetsRequirements(n, name, requiredNamespaceParts)).ToList();
 
-            if (foundName == null)
+            if (foundNames.Count > 1)
+            {
+                throw new InvalidOperationException(MultipleMatchingErrorMessage(name));
+            }
+            if (foundNames.Count < 1)
             {
                 throw new InvalidOperationException(ResourceNotFoundErrorMessage(name));
             }
 
-            return foundName;
+            return foundNames.Single();
         }
 
         private static bool MeetsRequirements(string canditate, string name, IEnumerable<string> requiredNamespaceParts)
@@ -38,6 +42,16 @@
                 string.Format(
                     "The resource with name '{0}' that matches the given namesspace could not be found in the assembly '{2}'. "
                     + "Did you set the 'Build Action' property to 'Embedded Resource'? \n\nFound resources: {1}",
+                    name,
+                    _sourceAssembly.GetManifestResourceNames().Aggregate("", (accu, s) => accu + "\n" + s),
+                    _sourceAssembly.FullName);
+        }
+
+        private string MultipleMatchingErrorMessage(string name)
+        {
+            return
+                string.Format(
+                    "Multiple matches for name '{0}' were found in the assembly '{2}'. \n\nFound resources: {1}",
                     name,
                     _sourceAssembly.GetManifestResourceNames().Aggregate("", (accu, s) => accu + "\n" + s),
                     _sourceAssembly.FullName);
